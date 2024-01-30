eca57aa45509977233a912f944dd795b1a2e91a3
Nate Ensign <dixienate@gmail.com>
2024-01-30 14:13:05 -0700

    can now delete projects

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,16 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      };
+    });
+  }
+
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return {
@@ -55,7 +65,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject}/>
+  let content = <SelectedProject project={selectedProject} deleteProject={handleDeleteProject} />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
