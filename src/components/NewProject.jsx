d712881419d042abe9fe9927306670e586815ddb
Nate Ensign <dixienate@gmail.com>
2024-01-30 12:46:50 -0700

    add task to sidebard functional

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import Input from './Input'
+import React, { useRef } from "react";
+import Input from "./Input";
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+        title: enteredTitle,
+        description: enteredDescription,
+        dueDate: enteredDueDate,
+    })
+  }
+
   return (
-    <div className='w-[35rem] mt-16'>
-        <menu className='flex items-center justify-end gap-4 my-4'>
-            <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-            <li><button className='px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-600'>Save</button></li>
-        </menu>
-        <div>
-          <Input label='Title'/>
-          <Input label='Description' textarea />
-          <Input label='Due Date' /> 
-        </div>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-600"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type='text' ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type='date' ref={dueDate} label="Due Date" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewProject
+export default NewProject;
